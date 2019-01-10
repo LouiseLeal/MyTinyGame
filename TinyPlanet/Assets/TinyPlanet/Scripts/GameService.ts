6a264f8a50bbd4e7bed47ed18ed1f8755e7bcf5c
Louise leal <lcloudine@gmail.com>
2019-01-09 22:38:09 -0400

    Download needed packages and render a sprite in the sceen

---
+++ TinyPlanet/Assets/TinyPlanet/Scripts/GameService.ts
@@ -0,0 +1,19 @@
+
+namespace game {
+
+    /** New System */
+    export class GameService{
+        //start new game
+        static newGame(world: ut.World) {
+            console.log("NewGame")
+
+            //Instantiate new entity group
+            ut.EntityGroup.instantiate(world, "game.GameScene");
+
+            //setup the initial state for the game
+           let config = world.getConfigData(game.GameConfig);
+
+        }
+      
+    }
+}
